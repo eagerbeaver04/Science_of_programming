e33d6af3a244e1a9adf8bb99a4fabdc41e1018b6
eagerbeaver04 <mike.markov04@gmail.com>
2023-11-15 17:48:04 +0300

    Add registration command

--- Wrapper/Engine.h
+++ Wrapper/Engine.h
@@ -7,5 +7,12 @@ private:
 	using argumentList = std::unordered_map<std::string, int>;
 	std::unordered_map < std::string, std::function<int(argumentList)>> commands;
 public:
-
+	template<typename T, typename... Pairs>
+	void registerCommand(Wrapper<T, Pairs...>* instance, const std::string& name)
+	{
+		commands[name] = [instance](const argumentList& arguments)
+		{
+			return instance(arguments);
+		};
+	}
 };
