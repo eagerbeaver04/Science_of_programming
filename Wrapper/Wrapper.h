15daaef6212a226f75e41f22551f8f48218700d3
eagerbeaver04 <mike.markov04@gmail.com>
2023-11-15 23:17:48 +0300

    Remove useless references

--- Wrapper/Wrapper.h
+++ Wrapper/Wrapper.h
@@ -5,13 +5,19 @@
 #include <functional>
 #include <variant>
 
+template <typename T>
+bool defaultValue(const T& value) 
+{
+    return T();
+}
+
 template <typename T, typename... Pairs>
 class Wrapper
 {
 private:
     using Fptr = int (T::*)(Pairs...);
     using argumentsList = std::unordered_map<std::string, int>;
-    T *instancePointer;
+    T* instancePointer;
     Fptr methodPointer;
     argumentsList arguments;
 
@@ -21,29 +27,28 @@ private:
     auto calculate(std::index_sequence<Is...>)
     {
         auto value = (instancePointer->*methodPointer)(values[Is]...);
-        for (auto &&el : values)
-            el = 0;
+        for (auto&& el : values)
+            el = defaultValue(el);
         return value;
     };
 
 public:
-    Wrapper(T *instancePointer_, Fptr methodPointer_, argumentsList arguments_) : instancePointer(instancePointer_),
-                                                                                  methodPointer(methodPointer_)
+    Wrapper(T* instancePointer_, Fptr methodPointer_, argumentsList arguments_) : instancePointer(instancePointer_),
+        methodPointer(methodPointer_)
     {
-        for (auto &&pair : arguments_)
+        for (auto&& pair : arguments_)
         {
             arguments[pair.first] = pair.second;
             values.push_back(pair.second);
         }
     }
 
-    auto execute(const argumentsList &arguments_)
+    auto execute(const argumentsList& arguments_)
     {
-        auto &&itStoreBegin = arguments.begin();
-        auto &&itStoreEnd = arguments.end();
-        auto &&itInput = arguments_.begin();
-        auto &&itStore = itStoreBegin;
-        int index;
+        auto itStoreBegin = arguments.begin();
+        auto itStoreEnd = arguments.end();
+        auto itInput = arguments_.begin();
+        auto itStore = itStoreBegin;
         for (int i = 0; i < values.size(); i++)
         {
             itStore = arguments.find(itInput->first);
@@ -52,8 +57,7 @@ public:
                 std::cerr << "Undefined argument" << std::endl;
                 return 0;
             }
-            index = std::distance(itStoreBegin, itStore);
-            values[index] = itInput->second;
+            values[std::distance(itStoreBegin, itStore)] = itInput->second;
             itInput++;
         }
         return calculate(std::make_index_sequence<sizeof...(Pairs)>{});
